a2a52a7ed03abd97f960e5355825d41a19124048
ricardoramos <ricardoramos@icmc.usp.br>
2015-07-16 17:12:44 -0300

    Criando a classe de teste CalculoFinalTest

--- test/br/com/caelum/tdd/CalculoFinalTest.java
+++ test/br/com/caelum/tdd/CalculoFinalTest.java
@@ -27,39 +27,15 @@ public class CalculoFinalTest {
 	}
 	
 	@Test
-	public void testeCalculoFinalFreteSPAte3ItensEDescontoEntre501E2000(){
+	public void testeCalculoFinalDeUmaCompra(){
 		Cliente cliente = new Cliente("Ricardo", Estado.SP);
-		Item item1 = new Item("Iphone 4", 900, 1);			
-		Compra compra = new Compra(Arrays.asList(item1), cliente);
-		// Valor do produto = 900
+		Item item1 = new Item("Iphone 4", 800, 1);	
+		Item item2 = new Item("Macbook", 1200, 1);			
+		Compra compra = new Compra(Arrays.asList(item1, item2), cliente);
+		// Valor total dos produtos = 2000
 		// Valor do frete = 10 (Até 3 itens)
-		// Valor do desconto (Valor do produto * percentual de desconto) = (900 * 0.10)
-		assertEquals((900 + 10 - (900 * 0.10)), calculoFinal.calcula(compra), 0.0001);
+		// Valor do desconto (Valor do produto * percentual de desconto) = (2000 * 0.10)
+		assertEquals((2000 + 10 - (2000 * 0.10)), calculoFinal.calcula(compra), 0.0001);
 	}
-	
-	@Test
-	public void testeCalculoFinalFreteSPCom3ItensEDescontoAcimaDe2000(){
-		Cliente cliente = new Cliente("Mauricio", Estado.SP);
-		Item item1 = new Item("Iphone 4", 900, 3);	
-		Item item2 = new Item("Macbook", 1200, 2);	
-		Item item3 = new Item("TV 52 Polegadas", 3200, 1);	
-		Compra compra = new Compra(Arrays.asList(item1, item2, item3), cliente);
-		// Valor total de todos os produtos -> 900*3 + 1200*2 + 3200*1 = 8300
-		// Valor do frete = 10 (Até 3 itens)
-		// Valor do desconto (Valor do produto * percentual de desconto) = (8300 * 0.15)
-		assertEquals((8300 + 10 - (8300 * 0.15)), calculoFinal.calcula(compra), 0.0001);
-	}
-	
-	@Test
-	public void testeCalculoFinalFreteSPComMaisDe3ItensEDescontoAcimaDe2000(){
-		Cliente cliente = new Cliente("Pedro", Estado.SP);
-		Item item1 = new Item("Iphone 4", 900, 1);	
-		Item item2 = new Item("Macbook", 1300, 1);	
-		Item item3 = new Item("TV 30 Polegadas", 1100, 1);	
-		Item item4 = new Item("DVD Philips", 175, 1);	
-		Compra compra = new Compra(Arrays.asList(item1, item2, item3, item4), cliente);
-		assertEquals((3475 + 17 - (3475 * 0.15)), calculoFinal.calcula(compra), 0.0001);
-	}
-	
 	
 }
